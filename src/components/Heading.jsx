3c777e97979f2f05ca0da525bcd932fdcdc3adc0
piyushyadavpy <piyushyadav4991@gmail.com>
2024-10-17 22:42:33 +0530

    benfits section done

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = ({ className, title }) => {
+  return (
+    <div className={`${className}`}>
+      {title && <h2 className="h2">{title}</h2>}
+    </div>
+  );
+};
+
+export default Heading;
